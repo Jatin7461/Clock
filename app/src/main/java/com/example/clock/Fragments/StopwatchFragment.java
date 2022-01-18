f8e0f5776714df955350fefc6b37878a76bb2257
Jatin Gandhi <jatingandhi7461@gmail.com>
2022-01-19 01:02:14 +0530

    start button works for stopwatch

--- app/src/main/java/com/example/clock/Fragments/StopwatchFragment.java
+++ app/src/main/java/com/example/clock/Fragments/StopwatchFragment.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.clock.R;
@@ -18,26 +21,65 @@ public class StopwatchFragment extends Fragment {
     private Button mStartButton, mResetButton;
 
     View view;
+    private long startTime = 0, updateTime = 0, timeGap = 0, timeinMilli = 0;
+
+    TextView screen;
+    final Handler handler = new Handler();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_stopwatch, container, false);
-        return view;
-    }
+        screen = (TextView) view.findViewById(R.id.stopwatch_screen);
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         mStartButton = view.findViewById(R.id.start_stopwatch);
         mResetButton = view.findViewById(R.id.reset_stopwatch);
         mStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getContext(), "Stopwatch Started", Toast.LENGTH_SHORT).show();
+                startStopwatch();
             }
         });
+        mResetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getContext(), "Stopwatch Stopped", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
+
+    private void startStopwatch() {
+
+        startTime = SystemClock.uptimeMillis();
+        handler.post(new Runnable() {
+            long milliseconds = 0;
+
+            @Override
+            public void run() {
+                timeinMilli = SystemClock.uptimeMillis() - startTime + 3599000 * 24;
+                updateTime = timeGap + timeinMilli;
+                int seconds = (int) (updateTime / 1000);
+                milliseconds = updateTime % 1000;
+                int minutes = seconds / 60;
+                int hours = (minutes / 60) % 24;
+                int realSeconds = seconds % 60;
+                int realMinutes = minutes % 60;
+                screen.setText(String.format("%02d", hours) + ":" + String.format("%02d", realMinutes) + ":" + String.format("%02d", realSeconds) + ":" + String.format("%03d", milliseconds));
+
+
+                handler.postDelayed(this, 0);
+
+            }
+        });
+    }
+
+    private void pauseStopwatch() {
+        timeGap += timeinMilli;
+
 
     }
 }
