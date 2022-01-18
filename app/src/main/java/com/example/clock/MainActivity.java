1f060ee7d15982181c6f870dde0118cc0209d11e
Jatin Gandhi <jatingandhi7461@gmail.com>
2022-01-18 16:42:49 +0530

    added a toolbar in alarmFragment

--- app/src/main/java/com/example/clock/MainActivity.java
+++ app/src/main/java/com/example/clock/MainActivity.java
@@ -15,12 +15,13 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment, AlarmFragment.class, null)
+                .commit();
 
 
     }
