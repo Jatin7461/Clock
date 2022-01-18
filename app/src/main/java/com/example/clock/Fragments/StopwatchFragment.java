f57f496c190833ed259e76b2805dbf14be5c7e38
Jatin Gandhi <jatingandhi7461@gmail.com>
2022-01-19 01:15:52 +0530

    added pause functionality in stopwatch

--- app/src/main/java/com/example/clock/Fragments/StopwatchFragment.java
+++ app/src/main/java/com/example/clock/Fragments/StopwatchFragment.java
@@ -19,12 +19,33 @@ import com.example.clock.R;
 public class StopwatchFragment extends Fragment {
 
     private Button mStartButton, mResetButton;
-
     View view;
     private long startTime = 0, updateTime = 0, timeGap = 0, timeinMilli = 0;
-
     TextView screen;
     final Handler handler = new Handler();
+    private boolean startPause;
+
+
+    Runnable r = new Runnable() {
+        long milliseconds = 0;
+
+        @Override
+        public void run() {
+            timeinMilli = SystemClock.uptimeMillis() - startTime;
+            updateTime = timeGap + timeinMilli;
+            int seconds = (int) (updateTime / 1000);
+            milliseconds = updateTime % 1000;
+            int minutes = seconds / 60;
+            int hours = (minutes / 60) % 24;
+            int realSeconds = seconds % 60;
+            int realMinutes = minutes % 60;
+            screen.setText(String.format("%02d", hours) + ":" + String.format("%02d", realMinutes) + ":" + String.format("%02d", realSeconds) + ":" + String.format("%03d", milliseconds));
+
+
+            handler.postDelayed(this, 0);
+
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,11 +56,28 @@ public class StopwatchFragment extends Fragment {
 
         mStartButton = view.findViewById(R.id.start_stopwatch);
         mResetButton = view.findViewById(R.id.reset_stopwatch);
+        startPause = true;
         mStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(), "Stopwatch Started", Toast.LENGTH_SHORT).show();
-                startStopwatch();
+
+                /**
+                 * if startPause is true
+                 */
+                if (startPause) {
+
+                    Toast.makeText(getContext(), "Stopwatch Started", Toast.LENGTH_SHORT).show();
+                    startStopwatch();
+                    mStartButton.setText(R.string.pause);
+                    startPause = false;
+                    mResetButton.setText(R.string.count);
+                } else {
+                    Toast.makeText(getContext(), "Stopwatch paused", Toast.LENGTH_SHORT).show();
+                    pauseStopwatch();
+                    mStartButton.setText(R.string.start);
+                    startPause = true;
+                    mResetButton.setText(R.string.reset);
+                }
             }
         });
         mResetButton.setOnClickListener(new View.OnClickListener() {
@@ -55,31 +93,33 @@ public class StopwatchFragment extends Fragment {
     private void startStopwatch() {
 
         startTime = SystemClock.uptimeMillis();
-        handler.post(new Runnable() {
-            long milliseconds = 0;
+//        handler.post(new Runnable() {
+//            long milliseconds = 0;
+//
+//            @Override
+//            public void run() {
+//                timeinMilli = SystemClock.uptimeMillis() - startTime + 3599000 * 24;
+//                updateTime = timeGap + timeinMilli;
+//                int seconds = (int) (updateTime / 1000);
+//                milliseconds = updateTime % 1000;
+//                int minutes = seconds / 60;
+//                int hours = (minutes / 60) % 24;
+//                int realSeconds = seconds % 60;
+//                int realMinutes = minutes % 60;
+//                screen.setText(String.format("%02d", hours) + ":" + String.format("%02d", realMinutes) + ":" + String.format("%02d", realSeconds) + ":" + String.format("%03d", milliseconds));
+//
+//
+//                handler.postDelayed(this, 0);
+//
+//            }
+//        });
 
-            @Override
-            public void run() {
-                timeinMilli = SystemClock.uptimeMillis() - startTime + 3599000 * 24;
-                updateTime = timeGap + timeinMilli;
-                int seconds = (int) (updateTime / 1000);
-                milliseconds = updateTime % 1000;
-                int minutes = seconds / 60;
-                int hours = (minutes / 60) % 24;
-                int realSeconds = seconds % 60;
-                int realMinutes = minutes % 60;
-                screen.setText(String.format("%02d", hours) + ":" + String.format("%02d", realMinutes) + ":" + String.format("%02d", realSeconds) + ":" + String.format("%03d", milliseconds));
-
-
-                handler.postDelayed(this, 0);
-
-            }
-        });
+        handler.post(r);
     }
 
     private void pauseStopwatch() {
         timeGap += timeinMilli;
-
+        handler.removeCallbacks(r);
 
     }
 }
