700ccf034eeecfe404d547ac74c37191b7f3afd8
Jatin Gandhi <jatingandhi7461@gmail.com>
2022-01-18 15:08:05 +0530

    added alarmAdapter class and alarms.xml to inflate views

---
+++ app/src/main/java/com/example/clock/AlarmAdapter.java
@@ -0,0 +1,52 @@
+package com.example.clock;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.zip.Inflater;
+
+public class AlarmAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+
+    public AlarmAdapter() {
+
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(R.layout.alarms, parent, false);
+
+        return new AlarmViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class AlarmViewHolder extends RecyclerView.ViewHolder {
+
+        public AlarmViewHolder(View view) {
+            super(view);
+
+        }
+
+    }
+
+
+}
