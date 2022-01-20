2bc0baafba479e08b4cdbfcf71eb5e8a7be83537
KaizokuJG <goldcoinjatin@gmail.com>
2022-01-20 21:47:25 +0530

    completed stopwatch code and started working on alarm

--- app/src/main/java/com/example/clock/Adapters/AlarmAdapter.java
+++ app/src/main/java/com/example/clock/Adapters/AlarmAdapter.java
@@ -4,50 +4,67 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.clock.R;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.zip.Inflater;
 
-public class AlarmAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class AlarmAdapter extends RecyclerView.Adapter<AlarmAdapter.AlarmViewHolder> {
 
+    public ArrayList<String> alarmList;
 
-    public AlarmAdapter() {
-
+    public AlarmAdapter(ArrayList<String> list) {
+        this.alarmList = list;
     }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
+    public AlarmViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
-
         View view = inflater.inflate(R.layout.alarms, parent, false);
+
 
         return new AlarmViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull AlarmViewHolder holder, int position) {
 
+        String s = alarmList.get(position);
+        holder.alarmTime.setText(s);
+        holder.alarmSwitch.toggle();
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return alarmList.size();
     }
 
     public class AlarmViewHolder extends RecyclerView.ViewHolder {
+        public TextView alarmTime;
+        public Switch alarmSwitch;
 
         public AlarmViewHolder(View view) {
             super(view);
-
+            alarmTime = view.findViewById(R.id.alarm_time);
+            alarmSwitch = view.findViewById(R.id.alarm_switch);
         }
 
+
+    }
+
+    public void addNewAlarm(ArrayList<String> list) {
+        this.alarmList = list;
+//        notifyItemChanged(alarmList.size() - 1);
+        notifyDataSetChanged();
     }
 
 
--- app/src/main/java/com/example/clock/Fragments/AlarmFragment.java
+++ app/src/main/java/com/example/clock/Fragments/AlarmFragment.java
@@ -3,48 +3,57 @@ package com.example.clock.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.clock.Adapters.AlarmAdapter;
 import com.example.clock.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AlarmFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class AlarmFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
     private FloatingActionButton addAlarm;
-
-
+    private RecyclerView recyclerView;
+    private AlarmAdapter adapter;
+    private ArrayList<String> list;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-
         View view = inflater.inflate(R.layout.fragment_alarm, container, false);
         addAlarm = view.findViewById(R.id.add_alarm);
 
+        //initialize recycler view and adapter
+        list = new ArrayList<>();
+        recyclerView = view.findViewById(R.id.alarm_list);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        adapter = new AlarmAdapter(list);
+        recyclerView.setAdapter(adapter);
+
+
+        //onclick listener for FAB
         addAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getContext(), "Add Alarm", Toast.LENGTH_SHORT).show();
+                list.add("9:00");
+                adapter.addNewAlarm(list);
             }
         });
 
 
         return view;
     }
+
+
 }
