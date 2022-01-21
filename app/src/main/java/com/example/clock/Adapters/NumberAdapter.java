26daab5e085bb33f127dc993e4f175e92ecc0cec
KaizokuJG <goldcoinjatin@gmail.com>
2022-01-21 19:31:46 +0530

    added two recycler views with infinite scrolls and an adapter class for the recycler views in activity_new_alarm.xml and NewAlarmActivity.java

---
+++ app/src/main/java/com/example/clock/Adapters/NumberAdapter.java
@@ -0,0 +1,57 @@
+package com.example.clock.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.clock.R;
+
+import java.util.ArrayList;
+
+public class NumberAdapter extends RecyclerView.Adapter<NumberAdapter.NumberAdapterViewHolder> {
+
+    private ArrayList<Integer> list = new ArrayList<>();
+
+    public NumberAdapter(ArrayList<Integer> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public NumberAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.number, parent, false);
+        return new NumberAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NumberAdapterViewHolder holder, int position) {
+        int realpos = position % list.size();
+        int num = list.get(realpos);
+        holder.numberView.setText(Integer.toString(num));
+    }
+
+    @Override
+    public int getItemCount() {
+        return Integer.MAX_VALUE;
+    }
+
+    public class NumberAdapterViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView numberView;
+
+        public NumberAdapterViewHolder(View view) {
+            super(view);
+            numberView = view.findViewById(R.id.number_list);
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/clock/NewAlarmActivity.java
+++ app/src/main/java/com/example/clock/NewAlarmActivity.java
@@ -1,15 +1,25 @@
 package com.example.clock;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.example.clock.Adapters.NumberAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NewAlarmActivity extends AppCompatActivity {
 
     private TextView toolbarTitle;
+    private ArrayList<Integer> hoursList;
+    private ArrayList<Integer> minutesList;
+    private RecyclerView hoursRecyclerView, minutesRecyclerView;
+    private NumberAdapter hoursAdapter, minutesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +27,28 @@ public class NewAlarmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_alarm);
         toolbarTitle = findViewById(R.id.toolbar_title);
         toolbarTitle.setTextColor(getResources().getColor(R.color.white));
+
+        hoursList = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23));
+        minutesList = new ArrayList<>();
+        for (int i = 0; i < 60; i++) {
+            minutesList.add(i);
+        }
+
+        LinearLayoutManager hoursLayoutManager = new LinearLayoutManager(this);
+
+        hoursRecyclerView = findViewById(R.id.hours_list);
+        hoursRecyclerView.setLayoutManager(hoursLayoutManager);
+        hoursAdapter = new NumberAdapter(hoursList);
+        hoursRecyclerView.setAdapter(hoursAdapter);
+        hoursRecyclerView.getLayoutManager().scrollToPosition(Integer.MAX_VALUE / 2);
+
+
+        LinearLayoutManager minLayoutManager = new LinearLayoutManager(this);
+        minutesRecyclerView = findViewById(R.id.minutes_list);
+        minutesRecyclerView.setLayoutManager(minLayoutManager);
+        minutesAdapter = new NumberAdapter(minutesList);
+        minutesRecyclerView.setAdapter(minutesAdapter);
+        minutesRecyclerView.getLayoutManager().scrollToPosition(Integer.MAX_VALUE / 2);
+
     }
 }
